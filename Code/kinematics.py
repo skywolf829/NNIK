1430141ba23be39cceac553e197cec4d042ee650
Skylar Wurster <swwurster@gmail.com>
2022-04-01 20:18:43 -0400

    Moves code to torch, adds grid sampling

--- Code/kinematics.py
+++ Code/kinematics.py
@@ -9,32 +9,32 @@ def config_to_matrix(p1,t1,xi):
     p1[p1 == 0] = 1e-10
 
     L=15/100
-    m0_0 = np.cos(t1 + math.pi / 3) ** 2 + np.sin(t1 + math.pi / 3) ** 2 - np.cos(t1 + math.pi / 3) ** 2 * xi ** 2 * p1 ** 2 / 2 + np.cos(t1 + math.pi / 3) ** 2 * xi ** 4 * p1 ** 4 / 24 - np.cos(t1 + math.pi / 3) ** 2 * xi ** 6 * p1 ** 6 / 720 + np.cos(t1 + math.pi / 3) ** 2 * xi ** 8 * p1 ** 8 / 40320 - np.cos(t1 + math.pi / 3) ** 2 * xi ** 10 * p1 ** 10 / 3628800 + np.cos(t1 + math.pi / 3) ** 2 * xi ** 12 * p1 ** 12 / 479001600 - np.cos(t1 + math.pi / 3) ** 2 * xi ** 14 * p1 ** 14 / 87178291200
-    m0_1 = np.cos(t1 + math.pi / 3) * xi ** 2 * p1 ** 2 * np.sin(t1 + math.pi / 3) / 2 - np.cos(t1 + math.pi / 3) * xi ** 4 * p1 ** 4 * np.sin(t1 + math.pi / 3) / 24 + np.cos(t1 + math.pi / 3) * xi ** 6 * p1 ** 6 * np.sin(t1 + math.pi / 3) / 720 - np.cos(t1 + math.pi / 3) * xi ** 8 * p1 ** 8 * np.sin(t1 + math.pi / 3) / 40320 + np.cos(t1 + math.pi / 3) * xi ** 10 * p1 ** 10 * np.sin(t1 + math.pi / 3) / 3628800 - np.cos(t1 + math.pi / 3) * xi ** 12 * p1 ** 12 * np.sin(t1 + math.pi / 3) / 479001600 + np.cos(t1 + math.pi / 3) * xi ** 14 * p1 ** 14 * np.sin(t1 + math.pi / 3) / 87178291200
-    m0_2 = -np.cos(t1 + math.pi / 3) * xi * p1 + np.cos(t1 + math.pi / 3) * xi ** 3 * p1 ** 3 / 6 - np.cos(t1 + math.pi / 3) * xi ** 5 * p1 ** 5 / 120 + np.cos(t1 + math.pi / 3) * xi ** 7 * p1 ** 7 / 5040 - np.cos(t1 + math.pi / 3) * xi ** 9 * p1 ** 9 / 362880 + np.cos(t1 + math.pi / 3) * xi ** 11 * p1 ** 11 / 39916800 - np.cos(t1 + math.pi / 3) * xi ** 13 * p1 ** 13 / 6227020800
-    m0_3 = -np.cos(t1 + math.pi / 3) * xi ** 2 * p1 * L / 2 + np.cos(t1 + math.pi / 3) * xi ** 4 * p1 ** 3 * L / 24 - np.cos(t1 + math.pi / 3) * xi ** 6 * p1 ** 5 * L / 720 + np.cos(t1 + math.pi / 3) * xi ** 8 * p1 ** 7 * L / 40320 - np.cos(t1 + math.pi / 3) * xi ** 10 * p1 ** 9 * L / 3628800 + np.cos(t1 + math.pi / 3) * xi ** 12 * p1 ** 11 * L / 479001600 - np.cos(t1 + math.pi / 3) * xi ** 14 * p1 ** 13 * L / 87178291200
-    m1_0 = np.cos(t1 + math.pi / 3) * xi ** 2 * p1 ** 2 * np.sin(t1 + math.pi / 3) / 2 - np.cos(t1 + math.pi / 3) * xi ** 4 * p1 ** 4 * np.sin(t1 + math.pi / 3) / 24 + np.cos(t1 + math.pi / 3) * xi ** 6 * p1 ** 6 * np.sin(t1 + math.pi / 3) / 720 - np.cos(t1 + math.pi / 3) * xi ** 8 * p1 ** 8 * np.sin(t1 + math.pi / 3) / 40320 + np.cos(t1 + math.pi / 3) * xi ** 10 * p1 ** 10 * np.sin(t1 + math.pi / 3) / 3628800 - np.cos(t1 + math.pi / 3) * xi ** 12 * p1 ** 12 * np.sin(t1 + math.pi / 3) / 479001600 + np.cos(t1 + math.pi / 3) * xi ** 14 * p1 ** 14 * np.sin(t1 + math.pi / 3) / 87178291200
-    m1_1 = np.cos(t1 + math.pi / 3) ** 2 + np.sin(t1 + math.pi / 3) ** 2 - np.sin(t1 + math.pi / 3) ** 2 * xi ** 2 * p1 ** 2 / 2 + np.sin(t1 + math.pi / 3) ** 2 * xi ** 4 * p1 ** 4 / 24 - np.sin(t1 + math.pi / 3) ** 2 * xi ** 6 * p1 ** 6 / 720 + np.sin(t1 + math.pi / 3) ** 2 * xi ** 8 * p1 ** 8 / 40320 - np.sin(t1 + math.pi / 3) ** 2 * xi ** 10 * p1 ** 10 / 3628800 + np.sin(t1 + math.pi / 3) ** 2 * xi ** 12 * p1 ** 12 / 479001600 - np.sin(t1 + math.pi / 3) ** 2 * xi ** 14 * p1 ** 14 / 87178291200
-    m1_2 = np.sin(t1 + math.pi / 3) * xi * p1 - np.sin(t1 + math.pi / 3) * xi ** 3 * p1 ** 3 / 6 + np.sin(t1 + math.pi / 3) * xi ** 5 * p1 ** 5 / 120 - np.sin(t1 + math.pi / 3) * xi ** 7 * p1 ** 7 / 5040 + np.sin(t1 + math.pi / 3) * xi ** 9 * p1 ** 9 / 362880 - np.sin(t1 + math.pi / 3) * xi ** 11 * p1 ** 11 / 39916800 + np.sin(t1 + math.pi / 3) * xi ** 13 * p1 ** 13 / 6227020800
-    m1_3 = np.sin(t1 + math.pi / 3) * xi ** 2 * p1 * L / 2 - np.sin(t1 + math.pi / 3) * xi ** 4 * p1 ** 3 * L / 24 + np.sin(t1 + math.pi / 3) * xi ** 6 * p1 ** 5 * L / 720 - np.sin(t1 + math.pi / 3) * xi ** 8 * p1 ** 7 * L / 40320 + np.sin(t1 + math.pi / 3) * xi ** 10 * p1 ** 9 * L / 3628800 - np.sin(t1 + math.pi / 3) * xi ** 12 * p1 ** 11 * L / 479001600 + np.sin(t1 + math.pi / 3) * xi ** 14 * p1 ** 13 * L / 87178291200
-    m2_0 = np.cos(t1 + math.pi / 3) * xi * p1 - np.cos(t1 + math.pi / 3) * xi ** 3 * p1 ** 3 / 6 + np.cos(t1 + math.pi / 3) * xi ** 5 * p1 ** 5 / 120 - np.cos(t1 + math.pi / 3) * xi ** 7 * p1 ** 7 / 5040 + np.cos(t1 + math.pi / 3) * xi ** 9 * p1 ** 9 / 362880 - np.cos(t1 + math.pi / 3) * xi ** 11 * p1 ** 11 / 39916800 + np.cos(t1 + math.pi / 3) * xi ** 13 * p1 ** 13 / 6227020800
-    m2_1 = np.sin(t1 + math.pi / 3) * xi * p1 + np.sin(t1 + math.pi / 3) * xi ** 3 * p1 ** 3 / 6 - np.sin(t1 + math.pi / 3) * xi ** 5 * p1 ** 5 / 120 + np.sin(t1 + math.pi / 3) * xi ** 7 * p1 ** 7 / 5040 - np.sin(t1 + math.pi / 3) * xi ** 9 * p1 ** 9 / 362880 + np.sin(t1 + math.pi / 3) * xi ** 11 * p1 ** 11 / 39916800 - np.sin(t1 + math.pi / 3) * xi ** 13 * p1 ** 13 / 6227020800
+    m0_0 = torch.cos(t1 + math.pi / 3) ** 2 + torch.sin(t1 + math.pi / 3) ** 2 - torch.cos(t1 + math.pi / 3) ** 2 * xi ** 2 * p1 ** 2 / 2 + torch.cos(t1 + math.pi / 3) ** 2 * xi ** 4 * p1 ** 4 / 24 - torch.cos(t1 + math.pi / 3) ** 2 * xi ** 6 * p1 ** 6 / 720 + torch.cos(t1 + math.pi / 3) ** 2 * xi ** 8 * p1 ** 8 / 40320 - torch.cos(t1 + math.pi / 3) ** 2 * xi ** 10 * p1 ** 10 / 3628800 + torch.cos(t1 + math.pi / 3) ** 2 * xi ** 12 * p1 ** 12 / 479001600 - torch.cos(t1 + math.pi / 3) ** 2 * xi ** 14 * p1 ** 14 / 87178291200
+    m0_1 = torch.cos(t1 + math.pi / 3) * xi ** 2 * p1 ** 2 * torch.sin(t1 + math.pi / 3) / 2 - torch.cos(t1 + math.pi / 3) * xi ** 4 * p1 ** 4 * torch.sin(t1 + math.pi / 3) / 24 + torch.cos(t1 + math.pi / 3) * xi ** 6 * p1 ** 6 * torch.sin(t1 + math.pi / 3) / 720 - torch.cos(t1 + math.pi / 3) * xi ** 8 * p1 ** 8 * torch.sin(t1 + math.pi / 3) / 40320 + torch.cos(t1 + math.pi / 3) * xi ** 10 * p1 ** 10 * torch.sin(t1 + math.pi / 3) / 3628800 - torch.cos(t1 + math.pi / 3) * xi ** 12 * p1 ** 12 * torch.sin(t1 + math.pi / 3) / 479001600 + torch.cos(t1 + math.pi / 3) * xi ** 14 * p1 ** 14 * torch.sin(t1 + math.pi / 3) / 87178291200
+    m0_2 = -torch.cos(t1 + math.pi / 3) * xi * p1 + torch.cos(t1 + math.pi / 3) * xi ** 3 * p1 ** 3 / 6 - torch.cos(t1 + math.pi / 3) * xi ** 5 * p1 ** 5 / 120 + torch.cos(t1 + math.pi / 3) * xi ** 7 * p1 ** 7 / 5040 - torch.cos(t1 + math.pi / 3) * xi ** 9 * p1 ** 9 / 362880 + torch.cos(t1 + math.pi / 3) * xi ** 11 * p1 ** 11 / 39916800 - torch.cos(t1 + math.pi / 3) * xi ** 13 * p1 ** 13 / 6227020800
+    m0_3 = -torch.cos(t1 + math.pi / 3) * xi ** 2 * p1 * L / 2 + torch.cos(t1 + math.pi / 3) * xi ** 4 * p1 ** 3 * L / 24 - torch.cos(t1 + math.pi / 3) * xi ** 6 * p1 ** 5 * L / 720 + torch.cos(t1 + math.pi / 3) * xi ** 8 * p1 ** 7 * L / 40320 - torch.cos(t1 + math.pi / 3) * xi ** 10 * p1 ** 9 * L / 3628800 + torch.cos(t1 + math.pi / 3) * xi ** 12 * p1 ** 11 * L / 479001600 - torch.cos(t1 + math.pi / 3) * xi ** 14 * p1 ** 13 * L / 87178291200
+    m1_0 = torch.cos(t1 + math.pi / 3) * xi ** 2 * p1 ** 2 * torch.sin(t1 + math.pi / 3) / 2 - torch.cos(t1 + math.pi / 3) * xi ** 4 * p1 ** 4 * torch.sin(t1 + math.pi / 3) / 24 + torch.cos(t1 + math.pi / 3) * xi ** 6 * p1 ** 6 * torch.sin(t1 + math.pi / 3) / 720 - torch.cos(t1 + math.pi / 3) * xi ** 8 * p1 ** 8 * torch.sin(t1 + math.pi / 3) / 40320 + torch.cos(t1 + math.pi / 3) * xi ** 10 * p1 ** 10 * torch.sin(t1 + math.pi / 3) / 3628800 - torch.cos(t1 + math.pi / 3) * xi ** 12 * p1 ** 12 * torch.sin(t1 + math.pi / 3) / 479001600 + torch.cos(t1 + math.pi / 3) * xi ** 14 * p1 ** 14 * torch.sin(t1 + math.pi / 3) / 87178291200
+    m1_1 = torch.cos(t1 + math.pi / 3) ** 2 + torch.sin(t1 + math.pi / 3) ** 2 - torch.sin(t1 + math.pi / 3) ** 2 * xi ** 2 * p1 ** 2 / 2 + torch.sin(t1 + math.pi / 3) ** 2 * xi ** 4 * p1 ** 4 / 24 - torch.sin(t1 + math.pi / 3) ** 2 * xi ** 6 * p1 ** 6 / 720 + torch.sin(t1 + math.pi / 3) ** 2 * xi ** 8 * p1 ** 8 / 40320 - torch.sin(t1 + math.pi / 3) ** 2 * xi ** 10 * p1 ** 10 / 3628800 + torch.sin(t1 + math.pi / 3) ** 2 * xi ** 12 * p1 ** 12 / 479001600 - torch.sin(t1 + math.pi / 3) ** 2 * xi ** 14 * p1 ** 14 / 87178291200
+    m1_2 = torch.sin(t1 + math.pi / 3) * xi * p1 - torch.sin(t1 + math.pi / 3) * xi ** 3 * p1 ** 3 / 6 + torch.sin(t1 + math.pi / 3) * xi ** 5 * p1 ** 5 / 120 - torch.sin(t1 + math.pi / 3) * xi ** 7 * p1 ** 7 / 5040 + torch.sin(t1 + math.pi / 3) * xi ** 9 * p1 ** 9 / 362880 - torch.sin(t1 + math.pi / 3) * xi ** 11 * p1 ** 11 / 39916800 + torch.sin(t1 + math.pi / 3) * xi ** 13 * p1 ** 13 / 6227020800
+    m1_3 = torch.sin(t1 + math.pi / 3) * xi ** 2 * p1 * L / 2 - torch.sin(t1 + math.pi / 3) * xi ** 4 * p1 ** 3 * L / 24 + torch.sin(t1 + math.pi / 3) * xi ** 6 * p1 ** 5 * L / 720 - torch.sin(t1 + math.pi / 3) * xi ** 8 * p1 ** 7 * L / 40320 + torch.sin(t1 + math.pi / 3) * xi ** 10 * p1 ** 9 * L / 3628800 - torch.sin(t1 + math.pi / 3) * xi ** 12 * p1 ** 11 * L / 479001600 + torch.sin(t1 + math.pi / 3) * xi ** 14 * p1 ** 13 * L / 87178291200
+    m2_0 = torch.cos(t1 + math.pi / 3) * xi * p1 - torch.cos(t1 + math.pi / 3) * xi ** 3 * p1 ** 3 / 6 + torch.cos(t1 + math.pi / 3) * xi ** 5 * p1 ** 5 / 120 - torch.cos(t1 + math.pi / 3) * xi ** 7 * p1 ** 7 / 5040 + torch.cos(t1 + math.pi / 3) * xi ** 9 * p1 ** 9 / 362880 - torch.cos(t1 + math.pi / 3) * xi ** 11 * p1 ** 11 / 39916800 + torch.cos(t1 + math.pi / 3) * xi ** 13 * p1 ** 13 / 6227020800
+    m2_1 = torch.sin(t1 + math.pi / 3) * xi * p1 + torch.sin(t1 + math.pi / 3) * xi ** 3 * p1 ** 3 / 6 - torch.sin(t1 + math.pi / 3) * xi ** 5 * p1 ** 5 / 120 + torch.sin(t1 + math.pi / 3) * xi ** 7 * p1 ** 7 / 5040 - torch.sin(t1 + math.pi / 3) * xi ** 9 * p1 ** 9 / 362880 + torch.sin(t1 + math.pi / 3) * xi ** 11 * p1 ** 11 / 39916800 - torch.sin(t1 + math.pi / 3) * xi ** 13 * p1 ** 13 / 6227020800
     m2_2 = 1 - xi ** 2 * p1 ** 2 / 2 + xi ** 4 * p1 ** 4 / 24 - xi ** 6 * p1 ** 6 / 720 + xi ** 8 * p1 ** 8 / 40320 - xi ** 10 * p1 ** 10 / 3628800 + xi ** 12 * p1 ** 12 / 479001600 - xi ** 14 * p1 ** 14 / 87178291200
     m2_3 = xi * L - xi ** 3 * p1 ** 2 * L / 6 + xi ** 5 * p1 ** 4 * L / 120 - xi ** 7 * p1 ** 6 * L / 5040 + xi ** 9 * p1 ** 8 * L / 362880 - xi ** 11 * p1 ** 10 * L / 39916800 + xi ** 13 * p1 ** 12 * L / 6227020800
-    m3_0 = np.zeros([p1.shape[0], 1])
-    m3_1 = np.zeros([p1.shape[0], 1])
-    m3_2 = np.zeros([p1.shape[0], 1])
-    m3_3 = np.ones([p1.shape[0], 1])
+    m3_0 = torch.zeros([p1.shape[0], 1], device=p1.device)
+    m3_1 = torch.zeros([p1.shape[0], 1], device=p1.device)
+    m3_2 = torch.zeros([p1.shape[0], 1], device=p1.device)
+    m3_3 = torch.ones([p1.shape[0], 1], device=p1.device)
     
-    res = np.concatenate((m0_0, m0_1, m0_2, m0_3, 
+    res = torch.cat((m0_0, m0_1, m0_2, m0_3, 
                           m1_0, m1_1, m1_2, m1_3,
                           m2_0, m2_1, m2_2, m2_3,
                           m3_0, m3_1, m3_2, m3_3),
-                         axis=1).reshape((p1.shape[0], 4, 4))
+                         dim=1).reshape((p1.shape[0], 4, 4))
 
     return res
 
-def intermediate_config_to_coord(p,xi_list=None):
+def intermediate_config_to_coord(p, xi_list=None):
     '''
     Takes a list of configurations p in the shape [n, 2*k], where
     n is the number of configurations and k is the number of arms of
@@ -46,7 +46,9 @@ def intermediate_config_to_coord(p,xi_list=None):
     
     # Assume we mean tip position if xi_list is None
     if(xi_list is None):
-        xi_list = np.ones([p.shape[0], 3])
+        xi_list = torch.ones([p.shape[0], 
+                            int(p.shape[1]/2)], 
+                            device=p.device)
     
     # Calcualte the matrices for each configuration
     m=config_to_matrix(p[:,0:1], p[:,1:2], xi_list[:,0:1])
@@ -56,20 +58,70 @@ def intermediate_config_to_coord(p,xi_list=None):
         m = m @ config_to_matrix(p[:,2*(i+1):2*(i+1)+1],
                                  p[:,2*(i+1)+1:2*(i+1)+2],
                                  xi_list[:,i+1:i+2])
+        
     # Return only the position at the end (rightmost column of matrix)
-    return m.transpose(0, 2, 1)[:,3,0:3]
+    return m.permute(0, 2, 1)[:,3,0:3]
 
-def random_configuration(num=1, segments=3):
+def random_configuration(num=1, segments=3, device="cpu"):
     '''
     Creates num random configurations of size [num, 2*segments]
     '''
-    configs = np.random.random(size=[num, 2*segments])
+    configs = torch.rand(size=[num, 2*segments], device=device)
     configs[::2] *= math.pi
     configs[1::2] *= math.pi * 2
     return configs
 
 
+def grid_configurations(phi_res = 128, theta_res = 128, segments=3, device="cpu"):
+    '''
+    Creates configurations in a uniform grid over phi and theta
+    with resolution for each phi_res and theta_res
+    '''
+    
+    p = torch.linspace(0, math.pi, phi_res, device=device)
+    t = torch.linspace(0, math.pi*2, theta_res, device=device)
+    
+    c = []
+    for _ in range(segments):
+        c.append(p.clone())
+        c.append(t.clone())
+    c = torch.meshgrid(c, indexing="ij")
+    c = torch.stack(c)
+    return c
+    
+def flatten_grid_config(g):
+    '''
+    Take a grid of [k*2, m, n, m, n, ..., m, n]
+    where k is the number of segments and m and n are the
+    theta and phi resolutions, respectively, and 
+    flattens all configurations into [(m*n)^k, k*2] for
+    batch style forward kinematics
+    '''
+    
+    return g.flatten(1).permute(1,0)
 
+def flattened_grid_result_to_grid(r, m, n, k):
+    '''
+    Takes a result in the shape [(m*n)^k, R],
+    where k is the number of segments and m and n are the
+    theta and phi resolutions, respectively, and 
+    R is the dimensionality of the result for each
+    configuration, and returns a grid of size
+    [R, m, n, m, n, ..., m, n]
+    '''
+    
+    s = [r.shape[1]]
+    for _ in range(k):
+        s.append(m)
+        s.append(n)
+    
+    return r.permute(1,0).reshape(s)
+    
+    
+    
+    
+    
+    
 
 
 
--- Code/main.py
+++ Code/main.py
@@ -1,17 +1,34 @@
-from kinematics import random_configuration, intermediate_config_to_coord
+from kinematics import *
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm 
 import time
 
 if __name__ == "__main__":
     
     t0 = time.time()
     np.random.seed(0)
+    device="cpu"
     
-    for _ in range(1):
-        r = random_configuration(10000)
-        p = intermediate_config_to_coord(r)
+    m = 128
+    n = 128
+    k = 1
+    
+    #c = random_configuration(num=1000000, segments=3, device=device)
+    c = grid_configurations(phi_res = m, theta_res = n,
+                            segments=k, device=device)
+    c = flatten_grid_config(c)
+    p = intermediate_config_to_coord(c)
     t_elapsed = time.time() - t0
-    print(p.shape)
-    print(t_elapsed)
-    #print(p)
+    print(f"Time elapsed: {t_elapsed : 0.05f}s")
+    
+    p = flattened_grid_result_to_grid(p, m, n, k)
+    p = p.norm(dim=0)
+    plt.pcolormesh(np.linspace(0, 2*math.pi, p.shape[1]),
+                   np.linspace(0, math.pi, p.shape[0]),
+                    p.clone().detach().cpu().numpy(),
+                    cmap=cm.gray)
+    plt.colorbar()
+    plt.xlabel("Theta")
+    plt.ylabel("Phi")
+    plt.show()
